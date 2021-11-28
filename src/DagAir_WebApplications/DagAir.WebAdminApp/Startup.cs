a846af5f8f827773f81d25de5927f329730e4d6a
fokamdk99 <stasskrzypek@gmail.com>
2021-11-28 18:49:18 +0100

    DAGAIR-24 Add authentication to WebAdminApp

---
+++ src/DagAir_WebApplications/DagAir.WebAdminApp/Startup.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using DagAir.Components.HealthChecks;
+using DagAir.WebAdminApp.Infrastructure.Swagger;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DagAir.WebAdminApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore()
+                .AddApiExplorer();
+            services.AddControllers();
+            services.AddConfiguredSwagger();
+            var healthChecksToBeDisabled = new List<string>();
+            healthChecksToBeDisabled.Add(HealthCheckFeature.RabbitMqHealthCheck);
+            services.AddDagAirHealthChecks(healthChecksToBeDisabled);
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(3);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;
+            });
+        }
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseConfiguredSwagger();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthCheckEndpoints();
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
