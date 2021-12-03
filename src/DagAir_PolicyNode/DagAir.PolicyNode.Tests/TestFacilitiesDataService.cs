7bbfe44277e2dfabbdc35b7d8c212d2f87ecc048
fokamdk99 <stasskrzypek@gmail.com>
2021-12-03 10:29:19 +0100

    DAGAIR-27 Fix broken test

---
+++ src/DagAir_PolicyNode/DagAir.PolicyNode.Tests/TestFacilitiesDataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using DagAir.Facilities.Contracts.DTOs;
+using DagAir.PolicyNode.Integrations.Facilities.DataServices;
+
+namespace DagAir.PolicyNode.Tests
+{
+    public class TestFacilitiesDataService : IFacilitiesDataService
+    {
+        public Task<RoomDto> GetRoomByRoomId(long roomId)
+        {
+            return Task.Run(() => CreateNewRoomDto(roomId));
+
+        }
+
+        private RoomDto CreateNewRoomDto(long roomId)
+        {
+            return new RoomDto
+            {
+                Id = roomId,
+                UniqueRoomId = Guid.NewGuid(),
+                Number = "133",
+                AffiliateId = 1
+            };
+        }
+    }
+}
